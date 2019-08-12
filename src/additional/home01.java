f79f49de3e628cf8d11f7090ff986674f85d30a8
luezair <dimonpiontkevich@gmail.com>
2019-08-12 05:32:20 +0300

    some bad commit with idea files

---
+++ src/additional/home01.java
@@ -0,0 +1,26 @@
+package additional;
+
+import java.util.Scanner;
+
+public class home01 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Сторона a: ");
+        int a = sc.nextInt();
+        System.out.print("Сторона b: ");
+        int b = sc.nextInt();
+        System.out.print("Сторона c: ");
+        int c = sc.nextInt();
+        byte d = 2;
+
+        if (Math.pow(a,d) == Math.pow(b,d) + Math.pow(c,d) ||
+                Math.pow(b,d) == Math.pow(a,d) + Math.pow(c,d) ||
+                Math.pow(c,d) == Math.pow(a,d) + Math.pow(b,d))  {
+            System.out.println("Трикутник прямокутний");
+        } else {
+            System.out.println("І шо ето такоє?");
+        }
+    }
+}
